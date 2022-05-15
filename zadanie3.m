bbb49cbfb4502eec7ff1e339fb67c8d427a00edd
KGrzybowski96 <101040054+KGrzybowski96@users.noreply.github.com>
2022-05-15 16:51:48 +0200

    Add files via upload

---
+++ zadanie3.m
@@ -0,0 +1,14 @@
+#Zwizualizuj funkcje aktywacji z poprzedniego zadania
+x = linspace[-1,1,100];
+y = relu(x);
+y2 = logistic(x);
+
+hold on
+plot(x,y, 'k--')
+plot(x, y2, 'r-')
+
+grid %siatka
+title('Wykresik')
+xlabel('oœ X')
+ylabel('oœ Y')
+legend('funkcja 1', 'funkcja 2')
